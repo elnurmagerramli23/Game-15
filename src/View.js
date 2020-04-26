ce29a5e41c668420a1dea01c921d70a2b6218c40
SevincMammadova <sevincmammadova609@gmail.com>
2020-04-26 15:14:50 +0400

    create div, heading, button

--- src/View.js
+++ src/View.js
@@ -2,4 +2,33 @@ function View() {
     this.root = document.querySelector('div#root');
 }
 
+const createDiv = params => {
+    const div = document.createElement('div');
+    div.setAttribute('class', params.class);
+    params.id && (div.id = params.id);
+    params.title && (div.title = params.title);
+    params.textContent && (div.textContent = params.textContent);
+    params.inner && (div.innerHTML = params.inner);
+
+    return div;
+}
+
+const createHeading = params => {
+    const heading = document.createElement('h1');
+    heading.setAttribute('class', params.class);
+    params.inner && (heading.innerHTML = params.inner);
+
+    return heading;
+}
+
+const createButton = params => {
+    const button = document.createElement('button');
+    button.setAttribute('class',params.class);
+    params.id && (button.id = params.id);
+    params.inner && (button.innerHTML = params.inner);
+    params.onclick && (button.onclick = params.onclick);
+
+    return button;
+}
+
 module.exports = View;
