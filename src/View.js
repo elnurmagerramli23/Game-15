1f592b43bd74afb5ca58304849c54aba29a9bb34
fara0000 <fexri_imanzade@mail.ru>
2020-04-26 15:13:31 +0400

    first half part of website is done!

--- src/View.js
+++ src/View.js
@@ -1,5 +1,22 @@
 function View() {
-    this.root = document.querySelector('div#root');
+    this._root = document.querySelector('div#root');
+}
+View.prototype.init = function() {
+    const container_welcome_page = createDiv({class: 'container_welcome_page'});
+    const container_game_page = createDiv({class: 'container_game_page'});
+    const welcome_page_bgColor = createDiv({class: 'welcome_page_bgColor'})
+    const page_heading = createHeading({class: 'page_heading'});
+    page_heading.innerHTML = 'WELCOME TO FIFTEEN!';
+    const play_button = createButton({class: 'play_button', id: 'play_button'});
+    play_button.innerHTML = 'PLAY';
+    const mix_button = createButton({class: 'mix_button', id: 'mix_button'});
+    mix_button.innerHTML = 'MIX';
+    this._root.append(container_welcome_page);
+    this._root.append(container_game_page);
+    container_welcome_page.append(page_heading);
+    container_welcome_page.append(play_button);
+    container_welcome_page.append(welcome_page_bgColor);
+    container_game_page.append(mix_button);
 }
 
 module.exports = View;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ let View = require("./View.js");
 let Controller = require("./Controller.js");
 
 function initialize() {
+    const view = new View(); 
+    const model = new Model(); 
+    const controller = new Controller(model, view);
 
+    controller.init();
 }
 
 initialize();
