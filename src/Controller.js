776c4a814b60296db586ea3614a3a9cb2c56a3be
elnurmagerramli23 <elnurmagerramli@gmail.com>
2020-05-08 09:55:34 +0400

    with Classes

--- src/Controller.js
+++ src/Controller.js
@@ -112,3 +112,126 @@ Controller.prototype.getVictory = function() {
 }
 
 export default Controller;
+
+// class Controller{
+// 	constructor(model, view) {
+// 		this._model = model;
+//     	this._view = view;
+//     	this._mtx = null;
+// 	}
+
+// 	init() {
+// 		this._view.init();
+// 	    this.sendMatrixToView();
+// 	    this.getRandom();
+// 	    this.getVictory();
+// 	    this._view.gameShuffle(this.shuffle.bind(this));
+// 	    this._view.sendValue(this.getFromModel.bind(this));
+// 	}
+
+// 	sendMatrixToView() {
+// 		const model = this._model.getMatrix();
+
+// 	    model.forEach(array => {
+// 	        array.forEach(element => {
+// 	            this._view.drawMatrix(element); 
+// 	        })
+// 	    })
+// 	}
+
+// 	getFromModel(value) {
+// 		const indexEl = this._model.findIndex(value);
+// 	    const check = this.checkForZero(indexEl);
+	    
+// 	    if(check) {
+// 	        this._model.swapElems(indexEl, check);
+// 	        console.log(this._model.getMatrix());
+	        
+// 	        this._view.deleteItems();
+// 	        this.sendMatrixToView();
+// 	        this.getVictory();
+// 	    }
+// 	}
+
+// 	checkForZero(item) {
+// 		const items = this._model.getMatrix();
+
+// 	    try {
+// 	        if (items[item.coordinateX + 1][item.coordinateY] === 0) {
+// 	            return { coordinateX: item.coordinateX + 1, coordinateY: item.coordinateY };
+// 	        }
+// 	    } catch (e) {
+// 	    }
+
+// 	    try {
+// 	        if (items[item.coordinateX - 1][item.coordinateY] === 0) {
+// 	            return { coordinateX: item.coordinateX - 1, coordinateY: item.coordinateY };
+// 	        }
+// 	    } catch (e) {
+// 	    }
+
+// 	    try {
+// 	        if (items[item.coordinateX][item.coordinateY + 1] === 0) {
+// 	            return { coordinateX: item.coordinateX, coordinateY: item.coordinateY + 1 };
+// 	        }
+// 	    } catch (e) {
+// 	    }
+
+// 	    try {
+// 	        if (items[item.coordinateX][item.coordinateY - 1] === 0) {
+// 	            return { coordinateX: item.coordinateX, coordinateY: item.coordinateY - 1 };
+// 	        }
+// 	    } catch (e) {
+// 	    }
+
+// 	    return false;
+// 	}
+
+// 	getRandom() {
+// 		this._mtx = this._model.getMatrix();
+// 	    let arr = this._mtx.flat();
+// 	    let temp = 0;
+// 	    let j = 0;
+
+// 	    for(let i = arr .length - 1; i > 0; i--){
+	        
+// 	        j = Math.floor(Math.random()* i + 0.5);
+// 	        temp = arr[j];
+// 	        arr[j] = arr[i];
+// 	        arr[i] = temp;
+// 	    }
+	    
+// 	    this._mtx = arr.map((_, i, a) => a.slice(i * 4, i * 4 + 4)).filter((el) => el.length);
+// 	}
+
+// 	shuffle() {
+// 		this.getRandom();
+// 	    this._model.setMatrix(this._mtx);
+// 	    this._view.deleteItems();    
+// 	    this.sendMatrixToView();
+// 	}
+
+// 	win() {
+// 		const arr = this._mtx;
+// 	    const matrix = this._model.getMatrix();
+
+// 	    for(let i = 0; i < arr.length; i++ ) {
+// 	        for(let j = 0; j < arr.length; j++) {
+// 	            if(arr[i][j] === matrix[i][j]) {
+// 	                alert('You did it bro!!!!');
+// 	            }
+// 	        }
+// 	    }
+// 	}
+
+// 	getVictory() {
+// 		const testMatrix = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+// 	    let testArray = this._mtx.flat();
+// 	    let test = JSON.stringify(testMatrix);
+// 	    let defaultArr = JSON.stringify(testArray);
+
+// 	    if(test === defaultArr) {
+// 	        setTimeout(function(){ alert('YOU DID IT BROOO!!!!'); }, 200);
+// 	    }
+// 	}
+// }
--- src/Model.js
+++ src/Model.js
@@ -59,3 +59,65 @@ Model.prototype.swapElems = function (elem1, elem2) {
 }
 
 export default Model;
+
+
+
+// class Model{
+// 	constructor() {
+// 		this._matrix = [
+// 	        [1, 2, 3, 4],
+// 	        [5, 6, 7, 8],
+// 	        [9, 10, 11, 12],
+// 	        [13, 14, 15, 0]
+// 	    ];
+	    
+// 	    this._box = {
+// 	        coordinateX: 0,
+// 	        coordinateY: 0,
+// 	        value: 0
+// 	    };
+	    
+// 	    this._zeroBox = {
+// 	        coordinateX: 0,
+// 	        coordinateY: 0,
+// 	        value: 0
+//         };
+// 	}
+
+// 	getMatrix() {
+// 		return this._matrix;
+// 	}
+
+// 	setMatrix(array) {
+// 		this._matrix = array;
+// 	}
+
+// 	findIndex(value) {
+// 		this._box.value = Number(value);
+    
+// 	    for(let i = 0; i < this._matrix.length; i++) {
+// 	        for(let j = 0; j < this._matrix.length; j++) {
+// 	            if(this._matrix[i][j] === this._box.value) {
+// 	                this._box.coordinateX = i;
+// 	                this._box.coordinateY = j;
+	    
+// 	                return this._box;
+// 	            }
+// 	        }
+// 	    }
+// 	}
+
+// 	swapElems(elem1, elem2) {
+// 		this._box = elem1;
+//     	this._zeroBox = elem2;
+	    
+// 	    [
+// 	        this._matrix[this._box.coordinateX][this._box.coordinateY],
+// 	        this._matrix[this._zeroBox.coordinateX][this._zeroBox.coordinateY]
+// 	    ] =
+// 	    [
+// 	        this._matrix[this._zeroBox.coordinateX][this._zeroBox.coordinateY],
+// 	        this._matrix[this._box.coordinateX][this._box.coordinateY]
+// 	    ]
+// 	} 
+// }
